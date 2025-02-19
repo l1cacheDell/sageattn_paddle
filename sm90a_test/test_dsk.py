ce8ff57a830fcb605696c22472bc6cd37c633635
l1cacheDell <d31409163@163.com>
2025-02-19 16:54:35 +0800

    dsk test

--- sm90a_test/test_dsk.py
+++ sm90a_test/test_dsk.py
@@ -1,5 +1,15 @@
 import paddle
 from paddle_sageattn import sageattn_qk_int8_pv_fp8_cuda_dsk_sm90 as sageattn_qk_int8_pv_fp8_cuda_sm90a_paddle
+from flash_attn_interface import flash_attn_func as flash_attn_func_v3
+
+from utils import precision_cmp_torch, precision_cmp
+
+import torch
+import paddle
+import numpy as np
+import os
+import argparse
+import nvtx
 
 bsz = 2
 seq_len = 1026
@@ -19,3 +29,4 @@ head_dim_og = head_dim_qk
 sm_scale = head_dim_og**-0.5
 
 o_paddle_sa = sageattn_qk_int8_pv_fp8_cuda_sm90a_paddle(q, k, v, tensor_layout=tensor_layout, is_causal=is_casual, qk_quant_gran="per_warp", return_lse=return_lse, pv_accum_dtype="fp32+fp32")
+paddle.device.synchronize()
